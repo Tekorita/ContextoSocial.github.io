4d0aa53e324f7201e68c06fccf4c4f11d750ac05
DavidPalma <davidpalmalugo@gmail.com>
2016-05-17 22:04:35 -0430

    Diseño de logos y correcion de errores en el fixed del navbar

---
+++ js/main.js
@@ -0,0 +1,42 @@
+(function(){
+		// Inicializamos el navbar
+		$(".button-collapse").sideNav();
+		// Inicializamos el dropdown
+		$('.dropdown-button').dropdown({
+		    inDuration: 500,
+		    outDuration: 500,
+		    constrain_width: true, // Does not change width of dropdown to that of the activator
+		    hover: true, // Activate on hover
+		    gutter: 0, // Spacing from edge
+		    belowOrigin: true, // Displays dropdown below the button
+		    alignment: 'left' // Displays dropdown with edge aligned to the left of button
+		    }
+		);
+		// Inicializamos el slideshow
+		  $(document).ready(function(){
+		      $('.slider').slider({full_width: true});
+		    });
+
+		  // Funcion para el menu fixed
+
+		  var loguito = document.getElementById("logo");
+
+		  $(document).ready(function(){
+		  		var altura = $('.menu').offset().top;
+		  		// alert(altura);
+
+		  		$(window).on('scroll', function(){
+		  			if ($(window).scrollTop() > altura ){
+		  				$('.menu').addClass("menu-fixed");
+		  				$('.menu').addClass("z-depth-2");
+		  				loguito.setAttribute("src", "imagenes/logo-nav.png");
+
+		  			}else{
+		  				$('.menu').removeClass("menu-fixed");
+		  				$('.menu').removeClass("z-depth-2");
+		  				loguito.setAttribute("src", "imagenes/logo-navv.png");
+		  			}
+
+		  		});
+		  });
+}());
